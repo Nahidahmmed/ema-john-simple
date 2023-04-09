25398bbe8812dd79a63d9af0f4241c96df7a0db5
Nahidahmmed <nahidahmmed411@gmail.com>
2023-04-09 16:56:58 +0600

    router

---
+++ src/components/Orders/Orders.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './Orders.css'
+import Cart from '../Cart/Cart';
+import { useLoaderData } from 'react-router-dom';
+import ReviewItem from '../ReviewItem.jsx/ReviewItem';
+import { removeFromDb } from '../../utilities/fakedb';
+
+const Orders = () => {
+    const savedCart = useLoaderData();
+    const [cart,setCart] = useState(savedCart);
+
+    const handleRemoveCart = (id) => {
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id)
+    }
+
+    return (
+        <div className='shop-container'>
+            <div className='order-container'>
+                {
+                    cart.map(product => <ReviewItem
+                    key={product.id}
+                    product={product}
+                    handleRemoveCart={handleRemoveCart}
+                    ></ReviewItem>)
+                }
+            </div>
+            <div className='cart-container'>
+                <Cart cart={cart}></Cart>
+            </div>
+        </div>
+    );
+};
+
+export default Orders;
